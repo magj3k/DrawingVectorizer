6cf9f98e042c6667ccfb5af9da2ec3e32e06c385
Magnus Johnson <magmhj@yahoo.com>
2021-10-03 20:42:41 -0700

    Vectorized images are now saved to default folder

--- test.py
+++ test.py
@@ -2,10 +2,11 @@ import sys
 from processors import *
 
 if __name__ == "__main__":
-    processor = PictureVectorizer(threshold=0.825, coeff=10.0, target_size=1400, stroke_width=0.5, color='#000', min_dist_threshold=3.2, close_trace_threshold=2.1)
+    processor = PictureVectorizer(threshold=0.835, coeff=10.0, target_size=1200, stroke_width=0.5, color='#000', min_dist_threshold=3.2, close_trace_threshold=2.1)
 
     if "." in sys.argv[1]:
         processor.process_img_at_path(sys.argv[1], output_path='test_out.svg')
         preview_np_image(load_np_image(sys.argv[1]), "test_in.png")
     else:
-        processor.process_batch(sys.argv[1], output_path="test")
+        new_path = sys.argv[1].strip("/")+"_vectorized"
+        processor.process_batch(sys.argv[1], output_path=new_path)
